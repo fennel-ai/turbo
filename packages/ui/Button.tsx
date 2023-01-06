1dfa518f2ab471e40df6dbb02c6ff0f2c5ce35b2
Luke Smetham <luke@smetham.dev>
2023-01-06 16:56:31 +0100

    Dummy button component

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -2,22 +2,43 @@ import * as React from 'react';
 
 interface Props {
 	primary?: boolean;
-	size?: "small" | "large";
 	label?: string;
+}
+
+const baseStyles = {
+	border: 0,
+	margin: 0,
+	padding: '0 0.75rem',
+	height: '2.5rem',
+	borderRadius: '0.5rem',
+	cursor: 'pointer',
+	fontSize: '0.875rem',
+	lineHeight: '1rem',
+	fontWeight: '500',
+	fontFamily: 'Satoshi Variable',
+};
+
+const styles = {
+	primary: {
+		...baseStyles,
+		backgroundColor: '#5D4CBE',
+		color: '#FFFFFF',
+		boxShadow: '0px 1px 2px rgba(72, 69, 81, 0.4), 0px 0px 0px 1px #5D4CBE, 0px 4px 6px rgba(72, 69, 81, 0.08)',
+	},
+	secondary: {
+		...baseStyles,
+		backgroundColor: '#E5DEFF',
+		color: '#190064',
+		boxShadow: '0px 1px 2px rgba(72, 69, 81, 0.12), 0px 0px 0px 1px #E5DEFF, 0px 4px 6px rgba(72, 69, 81, 0.04)'
+	}
 }
 
 export const Button = ({
 	primary = false,
-	label = "Boop",
-	size = "small",
+	label = "Label",
 }: Props) => {
 	return (
-		<button
-			style={{
-				backgroundColor: primary ? "red" : "blue",
-				fontSize: size === "large" ? "24px" : "14px",
-			}}
-		>
+		<button style={primary ? styles.primary : styles.secondary}>
 			{label}
 		</button>
 	);
