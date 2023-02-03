f47ba4b990d2f84bb92a0629e0f96b4e0b07129b
Luke Smetham <luke@smetham.dev>
2023-02-03 17:07:36 +0100

    added types for the navigation structure

--- apps/docs/lib/getNavigation.ts
+++ apps/docs/lib/getNavigation.ts
@@ -1,5 +1,23 @@
 import navigation from '../.content/navigation.json';
 
-export const getNavigation = () => {
+export type NavigationPage = {
+	title: string,
+	path: string,
+	slug: string,
+}
+
+export type NavigationSection = {
+	title: string,
+	slug: string,
+	order: number,
+	pages: NavigationPage[]
+}
+
+export type Navigation = NavigationSection[];
+
+/**
+ * Returns the navigation JSON to the client-side (created in the WithGitbookDocs plugin)
+ */
+export const getNavigation = (): Navigation => {
 	return navigation
 }
