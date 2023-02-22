28937b52b2988d037fb9f23818121c818e3a8487
Luke Smetham <luke@smetham.dev>
2023-02-22 11:56:23 +0100

    responsive style for h2 & h3

--- apps/docs/components/Layout/Layout.tsx
+++ apps/docs/components/Layout/Layout.tsx
@@ -48,6 +48,26 @@ const Root = styled(Container)`
 			}
 		}
 
+		h2 {
+			font-size: 2rem;
+			line-height: 2.5rem;
+
+			${media('sm')} {
+				font-size: 2.25rem;
+				line-height: 2.5rem
+			}
+		}
+		
+		h3 {
+			font-size: 1.5rem;
+			line-height: 2rem;
+
+			${media('sm')} {
+				font-size: 1.75rem;
+				line-height: 2.25rem
+			}
+		}
+
 		& > p {
 			margin: 0;
 			font-size: 1.125rem;
