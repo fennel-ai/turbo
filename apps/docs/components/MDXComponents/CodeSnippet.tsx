41e6b05c598ddb3fcd64259d528a6545d62ba807
Luke Smetham <luke@smetham.dev>
2023-02-22 23:28:02 +0100

    Update CodeSnippet.tsx

--- apps/docs/components/MDXComponents/CodeSnippet.tsx
+++ apps/docs/components/MDXComponents/CodeSnippet.tsx
@@ -1,5 +1,9 @@
+import { PropsWithChildren, ReactElement, useMemo } from "react";
 import { CodeBlock } from "ui";
 
-export const CodeSnippet = (props) => {
-	return <CodeBlock code={props.children.props.children} language="" />;
+export const CodeSnippet = (props: PropsWithChildren<{ className: string }>) => {
+	const code = props.children as ReactElement;
+	const language = useMemo(() => props.className?.replace("language-", "") || "python", [props.className]);
+	
+	return <CodeBlock code={code.props.children} language={language} />
 }
