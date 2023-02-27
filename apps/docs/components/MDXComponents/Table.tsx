e3b1c4c96d5f81f71d2154e2056608f8b9e19d7e
Luke Smetham <luke@smetham.dev>
2023-02-27 12:03:39 +0100

    Update Table.tsx

--- apps/docs/components/MDXComponents/Table.tsx
+++ apps/docs/components/MDXComponents/Table.tsx
@@ -1,10 +1,14 @@
-import { Children, PropsWithChildren, ReactElement, ReactNode, useMemo } from "react";
+import { Children, PropsWithChildren, ReactElement, ReactNode } from "react";
 import styled from '@emotion/styled';
 import { get } from "styles/utils";
 
 const Root = styled.table`
+	display: flex;
+	flex-direction: column;
 	border-collapse: separate;
 	margin: 1rem 0 2rem 0;
+	width: 100%;
+	overflow-x: auto;
 
 	& thead {
 		tr {
@@ -12,30 +16,89 @@ const Root = styled.table`
 			box-shadow: 0px 2px 0px ${get("border")};
 
 			th {
-				padding: 1rem 0;
+				padding: 1rem;
 				border-collapse: separate;
 				font-size: 1.125rem;
 				line-height: 1.5rem;
 				color: ${({ theme }) => theme["text-alt"] };
+
+				&:first-of-type {
+					padding-left: 0;
+				}
+
+				&:last-of-type {
+					padding-right: 0;
+				}
 			}
 		}
 	}
 
 	& tbody {
 		tr td {
-			padding: 1rem 0; 
+			padding: 1rem; 
 			font-size: 1rem;
 			line-height: 1.5rem;
 			font-variation-settings: "wght" ${get("fontWeights.medium")};
 			vertical-align: top;
+
+			&:first-of-type {
+				padding-left: 0;
+			}
+
+			&:last-of-type {
+				padding-right: 0;
+			}
 		}
 	}
 `;
 
-export const Table = (props: PropsWithChildren) => {
+const renderTable = (children: ReactElement) => Children.map(children, (child: ReactElement, i) => {
+	switch (child.type) {
+		case 'thead': {
+			return (
+				<thead key={i}>
+					{renderTable(child.props.children)}
+				</thead>
+			)
+		}
+		case 'tbody': {
+			return (
+				<tbody key={i}>
+					{renderTable(child.props.children)}
+				</tbody>
+			)
+		}
+		case 'tr': {
+			return (
+				<tr key={i}>
+					{renderTable(child.props.children)}
+				</tr>
+			)
+		}
+		case 'td': {
+			return (
+				<td key={i}>
+					{child.props.children}
+				</td>
+			);
+		}
+		case 'th': {
+			return (
+				<th key={i}>
+					{child.props.children}
+				</th>
+			);
+		}
+		default: {
+			return null;
+		}
+	}
+});
+
+export const Table = (props: { children: ReactElement }) => {
 	return (
 		<Root>
-			{props.children}
+			{renderTable(props.children)}
 		</Root>
 	)
 };
