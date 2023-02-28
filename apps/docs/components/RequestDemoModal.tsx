2d1cf662bfd1341cbfe0fa52cb822e26cd630b1b
Luke Smetham <luke@smetham.dev>
2023-02-28 18:07:24 +0100

    Build UI for the RequestDemoModal

--- apps/docs/components/RequestDemoModal.tsx
+++ apps/docs/components/RequestDemoModal.tsx
@@ -2,21 +2,12 @@ import { MouseEventHandler } from 'react';
 import { createPortal } from "react-dom";
 import styled from "@emotion/styled";
 import { motion } from 'framer-motion';
-import { IconButton } from 'ui';
+import { Button, IconButton } from 'ui';
 import CloseIcon from 'ui/icons/close.svg';
 import { media } from 'styles/utils';
 
 import { useModalPresence } from 'hooks/useModalPresence';
-
-const Sheet = styled(motion.div)`
-	position: fixed;
-	top: 0;
-	left: 0;
-	right: 0;
-	bottom: 0;
-	background-color: rgba(0, 0, 0, 0.64);
-	z-index: 5;
-`;
+import ModalSheet from './ModalSheet';
 
 const Root = styled(motion.div)`
 	position: fixed;
@@ -28,6 +19,9 @@ const Root = styled(motion.div)`
 	z-index: 5;
 	overflow-y: auto;
 	padding: 0.5rem 1rem;
+	display: flex;
+	flex-direction: column;
+	gap: 1rem;
 
 	${media('2xs')} {
 		top: 50%;
@@ -35,7 +29,7 @@ const Root = styled(motion.div)`
 		right: unset;
 		bottom: unset;
 		border-radius: 1.25rem;
-		padding: 1rem 2rem;
+		padding: 1.5rem;
 		max-width: 29rem;
 		width: 100%;
 	}
@@ -47,36 +41,99 @@ const Header = styled.div`
 	justify-content: space-between;
 `;
 
+const LogoWrapper = styled.div`
+	align-self: center;
+	width: 4rem;
+	height: 4rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	border-radius: 1rem;
+	box-shadow: 0px 0.4150884747505188px 2.2138051986694336px 0px hsla(260, 6%, 29%, 0.02), 0px 0.9975153207778931px 5.32008171081543px 0px hsla(260, 6%, 29%, 0.03), 0px 1.8782328367233276px 10.017241477966309px 0px hsla(260, 6%, 29%, 0.04), 0px 3.3504464626312256px 17.869047164916992px 0px hsla(260, 6%, 29%, 0.05),  0px 6.266641616821289px 33.422088623046875px 0px hsla(260, 6%, 29%, 0.06), 0px 15px 80px 0px hsla(260, 6%, 29%, 0.08);
+`;
+
+const Intro = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	gap: 0.25rem;
+	padding-left: 2rem;
+	padding-right: 2rem;
+
+	& h2 {
+		margin: 0;
+		font-size: 1.5rem;
+		line-height: 2rem;
+		font-variation-settings: 'wght' ${({ theme }) => theme.fontWeights.extrabold};
+	}
+
+	& p {
+		text-align: center;
+		margin: 0;
+		font-size: 0.875rem;
+		line-height: 1.25rem;
+		font-variation-settings: 'wght' ${({ theme }) => theme.fontWeights.medium};
+		color: ${({ theme }) => theme['text-alt']};
+	}
+`;
+
+const Form = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: stretch;
+	gap: 1rem;
+	padding-left: 1rem;
+	padding-right: 1rem;
+	padding-bottom: 1rem;
+	
+	& input {
+		height: 2.5rem;
+		border: 1px solid ${({ theme }) => theme['border']};
+		border-radius: 0.5rem;
+		outline: none;
+	}
+`;
+
+const SubmitButton = styled(Button)`
+	width: 100%;
+`;
+
 type Props = {
 	onClose: MouseEventHandler;
 }
 
 const ANIM = {
-	sheet: {
-		transition: { type: "spring", damping: 15, mass: 0.5, stiffness: 120 },
-		initial: { opacity: 0 },
-		animate: { opacity: 1 },
-		exit: { opacity: 0 },
-	},
-	root: {
-		transition: { type: "spring", damping: 15, mass: 0.2, stiffness: 200 },
-		initial: { y: 80, x: '-50%', opacity: 0 },
-		animate: { y: '-50%', opacity: 1 },
-		exit: { y: '-50%', scale: 0.97, opacity: 0, borderRadius: 24 },
-	}
+	transition: { type: "spring", damping: 15, mass: 0.2, stiffness: 200 },
+	initial: { y: '-45%', x: '-50%', opacity: 0 },
+	animate: { y: '-50%', opacity: 1 },
+	exit: { y: '-50%', scale: 0.97, opacity: 0, borderRadius: 24 },
 }
 
 const RequestDemoModal = (props: Props) => {
 	const { onClose } = props;
-	useModalPresence();
+	const isPresent = useModalPresence();
 
 	return createPortal(
 		<>
-			<Sheet initial={ANIM.sheet.initial} animate={ANIM.sheet.animate} exit={ANIM.sheet.exit} transition={ANIM.sheet.transition} onClick={onClose} />
-			<Root initial={ANIM.root.initial} animate={ANIM.root.animate} exit={ANIM.root.exit} transition={ANIM.root.transition}>
+			<ModalSheet isPresent={isPresent} onClick={onClose} />
+			<Root initial={ANIM.initial} animate={ANIM.animate} exit={ANIM.exit} transition={ANIM.transition}>
 				<Header>
 					<IconButton icon={CloseIcon} size="large" onClick={onClose} />
 				</Header>
+				<LogoWrapper>
+
+				</LogoWrapper>
+				<Intro>
+					<h2>Request a demo</h2>
+					<p>The team is excited to show you just how much Fennel is capable of; Submit a request today and we'll be in touch soon.</p>
+				</Intro>
+				<Form>
+					<input />
+					<input />
+					<input />
+					<SubmitButton label="Submit" variant="pill" />
+				</Form>
 			</Root>
 		</>,
 		document.body);
