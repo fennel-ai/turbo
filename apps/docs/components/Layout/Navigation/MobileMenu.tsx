340a5b13b3c0e87b46d8b292be92aee100749f83
Luke Smetham <luke@smetham.dev>
2023-03-01 12:13:57 +0100

    move font files on build

--- apps/docs/components/Layout/Navigation/MobileMenu.tsx
+++ apps/docs/components/Layout/Navigation/MobileMenu.tsx
@@ -33,7 +33,7 @@ const Root = styled(motion.div)`
 	padding: 0.5rem 1rem;
 	gap: 1.5rem;
 	box-shadow: 0px 18px 80px rgba(44, 45, 58, 0.04), 0px 7.51997px 33.4221px rgba(44, 45, 58, 0.0287542), 0px 4.02054px 17.869px rgba(44, 45, 58, 0.0238443), 0px 2.25388px 10.0172px rgba(44, 45, 58, 0.02), 0px 1.19702px 5.32008px rgba(44, 45, 58, 0.0161557), 0px 0.498106px 2.21381px rgba(44, 45, 58, 0.0112458);
-
+	
 	${media('2xs')} {
 		top: 1rem;
 		left: 1rem;
