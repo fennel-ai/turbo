93c98cda974a18b2bfc5a0a02892b4d782a8bf38
Luke Smetham <luke@smetham.dev>
2023-03-03 13:19:02 +0100

    remove local content and fetch over git on build

--- apps/docs/contentlayer.config.js
+++ apps/docs/contentlayer.config.js
@@ -1,7 +1,9 @@
-import { defineDocumentType, makeSource } from 'contentlayer/source-files';
+import { spawn } from "node:child_process";
+import { defineDocumentType } from "contentlayer/source-files";
+import { makeSource } from "contentlayer/source-remote-files";
 import remarkGfm from "remark-gfm";
 import remarkMdxDisableExplicitJsx from "remark-mdx-disable-explicit-jsx";
-import remarkDirective from 'remark-directive';
+import remarkDirective from "remark-directive";
 import rehypeImgSize from "rehype-img-size";
 import rehypeSlug from "rehype-slug";
 import remarkAdmonitions from "./plugins/remark-admonitions";
@@ -30,7 +32,7 @@ const Section = defineDocumentType(() => ({
 
 export const DocPage = defineDocumentType(() => ({
   name: "DocPage",
-  filePathPattern: "files/**/*.md",
+  filePathPattern: "pages/**/*.md",
   contentType: "mdx",
   fields: {
     title: {
@@ -39,9 +41,10 @@ export const DocPage = defineDocumentType(() => ({
     },
     status: {
       type: "enum",
-	  options: ['draft', 'wip', 'published'],
-      description: "Draft pages only appear in Development. WIP pages appear in Production but are marked as incomplete. Published pages are generated in Production and are publicly accessible.",
-	  default: 'draft',
+      options: ["draft", "wip", "published"],
+      description:
+        "Draft pages only appear in Development. WIP pages appear in Production but are marked as incomplete. Published pages are generated in Production and are publicly accessible.",
+      default: "draft",
     },
     description: {
       type: "string",
@@ -55,24 +58,78 @@ export const DocPage = defineDocumentType(() => ({
     order: {
       type: "number",
       description: "The order of the page in the navigation.",
-	  required: true,
+      required: true,
     },
   },
   computedFields: {
     slug: {
       type: "string",
-      resolve: (post) => post._raw.flattenedPath.replace(/files\/?/, ""),
+      resolve: (post) => post._raw.flattenedPath.replace(/pages\/?/, ""),
     },
     section: {
       type: "string",
-      resolve: (post) => post._raw.sourceFileDir.replace(/files\/?/, ""),
+      resolve: (post) => post._raw.sourceFileDir.replace(/pages\/?/, ""),
     },
   },
 }));
 
+const runBashCommand = (command) =>
+  new Promise((resolve, reject) => {
+    const child = spawn(command, [], { shell: true });
+
+    child.stdout.setEncoding("utf8");
+    child.stdout.on("data", (data) => process.stdout.write(data));
+
+    child.stderr.setEncoding("utf8");
+    child.stderr.on("data", (data) => process.stderr.write(data));
+
+    child.on("close", function (code) {
+      if (code === 0) {
+        resolve(void 0);
+      } else {
+        reject(new Error(`Command failed with exit code ${code}`));
+      }
+    });
+  });
+
+const REPO_URL = "https://github.com/fennel-ai/documentation-content.git";
+const CONTENT_DIR = ".content";
+const POLL = 0; // 1000 * 60
+
+const syncContent = async () => {
+  let cancelled = false;
+  let syncInterval;
+
+  const syncLoop = async () => {
+    console.log(`Pulling content from ${REPO_URL}`);
+
+    await runBashCommand(`
+      if [ -d  "${CONTENT_DIR}" ];
+        then
+          cd "${CONTENT_DIR}"; git pull;
+        else
+          git clone --depth 1 --single-branch ${REPO_URL} ${CONTENT_DIR};
+      fi
+    `);
+
+    if (cancelled || !POLL) return;
+
+    syncInterval = setTimeout(syncLoop, 3000);
+  };
+
+  await syncLoop();
+
+  return () => {
+    cancelled = true;
+    clearTimeout(syncInterval);
+  };
+};
+
 export default makeSource({
-  contentDirPath: "content",
+  syncFiles: syncContent,
+  contentDirPath: CONTENT_DIR,
   documentTypes: [DocPage, Section],
+  contentDirExclude: ['examples', '.git'],
   mdx: {
     remarkPlugins: [
       remarkMdxDisableExplicitJsx,
