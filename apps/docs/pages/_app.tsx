cd5bd19f11591754a27cba6f23647297944d7b1e
Luke Smetham <luke@smetham.dev>
2023-03-10 12:05:51 +0100

    Update _app.tsx

--- apps/docs/pages/_app.tsx
+++ apps/docs/pages/_app.tsx
@@ -1,11 +1,9 @@
-import { useEffect } from 'react';
 import { ThemeProvider } from '@emotion/react';
 import type { AppProps } from 'next/app'
 import Script from 'next/script';
 import theme from 'styles';
 import localFont from '@next/font/local';
 import { Toaster } from 'react-hot-toast';
-import { PostHogProvider } from 'posthog-js/react';
 import 'styles/index.css';
 import "@docsearch/css";
 
@@ -30,39 +28,37 @@ const toastOptions = {
 export default function App({ Component, pageProps }: AppProps) {
 	const router = useRouter();
 	return (
-		<PostHogProvider>
-			<ShellContextProvider>
-				<Script src={`${router.basePath}/posthog.js`} />
-				<style jsx global>
-					{`
-					@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600&display=swap');
+		<ShellContextProvider>
+			<Script src={`${router.basePath}/posthog.js`} />
+			<style jsx global>
+				{`
+				@import url('https://fonts.googleapis.com/css2?family=JetBrains+Mono:wght@600&display=swap');
 
-					* {
-						box-sizing: border-box;
-					}
+				* {
+					box-sizing: border-box;
+				}
 
-					body {
-						margin: 0;
-						padding: 0;
-						font-family: ${satoshiVariable.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-						font-synthesis: none;
-						font-feature-settings: "tnum"; /*! Enables the numeric character variants with Satoshi Variable - may want to restrict this to only li:marker elements */
-						text-rendering: optimizeLegibility;
-						-webkit-font-smoothing: antialiased;
-						-moz-osx-font-smoothing: grayscale;
-						-webkit-text-size-adjust: 100%;
-					}
+				body {
+					margin: 0;
+					padding: 0;
+					font-family: ${satoshiVariable.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+					font-synthesis: none;
+					font-feature-settings: "tnum"; /*! Enables the numeric character variants with Satoshi Variable - may want to restrict this to only li:marker elements */
+					text-rendering: optimizeLegibility;
+					-webkit-font-smoothing: antialiased;
+					-moz-osx-font-smoothing: grayscale;
+					-webkit-text-size-adjust: 100%;
+				}
 
-					button, input {
-						font-family: ${satoshiVariable.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
-					}
-				`}
-				</style>
-				<ThemeProvider theme={theme}>
-					<Component {...pageProps} />
-					<Toaster position="bottom-left" toastOptions={toastOptions} />
-				</ThemeProvider>
-			</ShellContextProvider>
-		</PostHogProvider>
+				button, input {
+					font-family: ${satoshiVariable.style.fontFamily}, system-ui, -apple-system, BlinkMacSystemFont, 'Helvetica Neue', Helvetica, Arial, sans-serif;
+				}
+			`}
+			</style>
+			<ThemeProvider theme={theme}>
+				<Component {...pageProps} />
+				<Toaster position="bottom-left" toastOptions={toastOptions} />
+			</ThemeProvider>
+		</ShellContextProvider>
 	)
 }
