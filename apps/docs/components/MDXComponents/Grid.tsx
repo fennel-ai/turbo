6518569bee17fac2f32bcb66151f87ad04010b11
Luke Smetham <luke@smetham.dev>
2023-03-10 21:52:30 +0100

    tweak grid & page reference styles

--- apps/docs/components/MDXComponents/Grid.tsx
+++ apps/docs/components/MDXComponents/Grid.tsx
@@ -1,10 +1,15 @@
 import { PropsWithChildren } from "react";
 import styled from "@emotion/styled";
+import { media } from "styles/utils";
 
 const Root = styled.div`
 	display: grid;
-	grid-template-columns: repeat(2, 1fr);
+	grid-template-columns: 1fr;
 	grid-gap: 2rem;
+
+	${media('sm')} {
+		grid-template-columns: repeat(2, 1fr);
+	}
 `;
 
 export const Grid = ({ children }: PropsWithChildren) => {
