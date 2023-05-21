531b4f3e3677ccf9fa80e3d1684d9f0f5868858b
Luke Smetham <luke@smetham.dev>
2023-05-21 13:09:05 +0100

    validate that all link targets exist

--- packages/remark-validate-href/src/index.ts
+++ packages/remark-validate-href/src/index.ts
@@ -1,5 +1,41 @@
-function remarkValidateHref(options: any, fileset: any) {
-	console.log(options, fileset);
-};
+import type { Plugin, Transformer } from 'unified';
+import fs from 'node:fs';
+import path from 'node:path';
+import { Visitor, visit } from 'unist-util-visit';
+import { Node } from 'mdast-util-to-string/lib';
+
+import { CONTENT_BASE } from "./constants";
+import { createContentMap } from './createContentMap';
+
+const contentMap = createContentMap();
+
+const remarkValidateHref: Plugin = (): Transformer => {
+	return (tree) => {
+		const visitor: Visitor<Node> = (node) => {
+			//@ts-ignore
+			let url = node.url as string;
+
+			const isExt = url.startsWith('http');
+
+			if (!isExt) {
+
+				let full_path = path.join(CONTENT_BASE, `${url}.md`);
+
+				if (contentMap[url]) {
+					full_path = contentMap[url];
+				}
+
+				try {
+					fs.accessSync(full_path);
+				} catch (err) {
+					// @ts-ignore
+					throw `Broken link: ${url} @ ${node.position.start.line}:${node.position.start.offset}`;
+				}
+			}
+		}
+
+		visit(tree, 'link', visitor)
+	}
+}
 
 export default remarkValidateHref;
