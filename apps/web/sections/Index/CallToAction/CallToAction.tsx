af9dd9d4ca5f26694c4135af2c4e1189c1e2ba16
Luke Smetham <luke@smetham.dev>
2023-06-06 14:07:17 +0100

    more responsive styles

--- apps/web/sections/Index/CallToAction/CallToAction.tsx
+++ apps/web/sections/Index/CallToAction/CallToAction.tsx
@@ -2,37 +2,55 @@ import styled from '@emotion/styled';
 import { Button, LinkButton, TitleBlock } from "ui";
 
 import { Container } from 'components/Container';
+import { media } from 'styles/utils';
 
 const Root = styled.div`
-	padding-top: 5rem;
+	padding-top: 2.5rem;
 	background-color: #140F2E;
 	color: #EBEBFA;
 	overflow: hidden;
+
+	${media('sm')} {
+		padding-top: 5rem;
+	}
 `;
 
 const Wrapper = styled(Container)`
 	display: grid;
 	grid-template-columns: repeat(12, 1fr);
-	row-gap: 7.5rem;
+	row-gap: 3rem;
 	margin-bottom: -4.5rem;
 
 	& button {
 		align-self: center;
 	}
+
+	${media('md')} {
+		row-gap: 7.5rem;
+	}
 `;
 
 const TitleWrapper = styled.div`
-	grid-column: 3 / span 8;
+	grid-column: span 12;
 	display: flex;
 	flex-direction: column;
 	gap: 1.5rem;
+
+	${media('md')} {
+		grid-column: 3 / span 8;
+	}
 `;
 
 const ConsoleImg = styled.div`
 	border-radius: 1.25rem;
 	background-color: #282150;
-	height: 27.5rem;
-	grid-column: 3 / span 8;
+	height: 16rem;
+	grid-column: span 12;
+
+	${media('md')} {
+		height: 27.5rem;
+		grid-column: 3 / span 8;
+	}
 `;
 
 const CallToAction = () => {
