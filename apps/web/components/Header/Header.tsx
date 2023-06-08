908e2663531fea64bdb9b1b16a443b09b708a8bb
Luke Smetham <luke@smetham.dev>
2023-06-08 12:31:13 +0100

    more sass refactoring

---
+++ apps/web/components/Header/Header.tsx
@@ -0,0 +1,19 @@
+import Logo from 'ui/icons/logo.svg';
+import styles from './Header.module.scss';
+
+export const Header = () => {
+	return (
+		<header className={styles.root}>
+			<div className={styles.backdrop} />
+			<nav>
+				<div className={styles.brand}>
+					<Logo />
+					<h2>Fennel</h2>
+				</div>
+			</nav>
+			<div className={styles.border}>
+				<div />
+			</div>
+		</header>
+	);
+};
---
+++ apps/web/components/SplitSection/SplitSection.tsx
@@ -0,0 +1,29 @@
+import { PropsWithChildren, ReactElement, StyleHTMLAttributes } from 'react';
+import { media } from 'styles/utils';
+import styled from '@emotion/styled';
+
+import { Container } from '../Container';
+import clsx from 'clsx';
+import styles from './SplitSection.module.scss';
+
+type Props = {
+	className?: string;
+	direction?: 'forward' | 'reverse';
+	illustration: ReactElement;
+	style?: StyleHTMLAttributes<HTMLDivElement>;
+}
+
+export const  SplitSection = ({ className, children, direction = "forward", illustration, style }: PropsWithChildren<Props>) => {
+	return (
+		<div className={clsx(styles.root, className)} style={style}>
+			<Container className={styles.wrapper}>
+				<div className={clsx(styles.illustration, styles[direction])}>
+					{illustration}
+				</div>
+				<div className={clsx(styles.content, styles[direction])}>
+					{children}
+				</div>
+			</Container>
+		</div>
+	)
+};
