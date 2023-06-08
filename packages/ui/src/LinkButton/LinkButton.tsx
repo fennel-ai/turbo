6d6a6994b51462083b9f469fcbf1b9a9ea12c881
Luke Smetham <luke@smetham.dev>
2023-06-08 13:55:08 +0100

    added state-layer mixin

---
+++ packages/ui/src/LinkButton/LinkButton.tsx
@@ -0,0 +1,16 @@
+import { HTMLAttributes, PropsWithChildren } from 'react';
+import clsx from 'clsx';
+import styles from './LinkButton.module.scss';
+
+import InternalLinkIcon from '../../icons/arrow-narrow-up-right.svg';
+
+export const LinkButton = (props: PropsWithChildren<HTMLAttributes<HTMLButtonElement>>) => {
+	const { className, children, style } = props;
+
+	return (
+		<button className={clsx(styles.root, className)} style={style}>
+			{children}
+			<InternalLinkIcon />
+		</button>
+	);
+};
