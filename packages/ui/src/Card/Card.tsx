b1dec78e557d657bdd131b59d14f6f8a970db42a
Luke Smetham <luke@smetham.dev>
2023-06-09 07:48:03 +0100

    refactor Card

--- packages/ui/src/Card/Card.tsx
+++ packages/ui/src/Card/Card.tsx
@@ -1,19 +1,12 @@
 import { ForwardedRef, HTMLAttributes, PropsWithChildren, StyleHTMLAttributes, forwardRef } from 'react';
-import styled from '@emotion/styled';
-
-const Root = styled.div`
-	background-color: rgba(240, 240, 245, 0.5);
-	backdrop-filter: blur(1rem);
-	border-radius: 1.5rem;
-	display: flex;
-	flex-direction: column;
-`;
+import clsx from 'clsx';
+import styles from './Card.module.scss';
 
 export const Card = forwardRef(({ className, children, style }: PropsWithChildren<HTMLAttributes<HTMLDivElement>>, ref: ForwardedRef<HTMLDivElement>) => {
 	return (
-		<Root className={className} ref={ref} style={style}>
+		<div className={clsx(styles.root, className)} ref={ref} style={style}>
 			{children}
-		</Root>
+		</div>
 	);
 });
 
