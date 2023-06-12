825e318f681cc955b669d0c06e090506d8aa674f
Luke Smetham <luke@smetham.dev>
2023-06-12 11:19:12 +0100

    useSectionTheme hook for /company

--- apps/web/pages/company.tsx
+++ apps/web/pages/company.tsx
@@ -1,7 +1,9 @@
+import { useSectionTheme } from "hooks/useSectionTheme";
 import { Angels, Hero, Investors, TeamMembers } from "sections/Company";
 import { CallToAction } from "sections/Index";
 
 export default function Company() {
+	useSectionTheme();
 	return (
 		<main>
 			<Hero />
