cd0db0c22f0c2c309c002b44c15983f4ad9d8ebd
Luke Smetham <luke@smetham.dev>
2023-06-21 09:30:32 +0200

    Update useScrollProgress.ts

--- packages/hooks/src/useScrollProgress.ts
+++ packages/hooks/src/useScrollProgress.ts
@@ -1,7 +1,13 @@
 import { useScroll, useTransform } from 'framer-motion';
+import type { MotionValue } from 'framer-motion';
 import useMeasure from 'react-use-measure';
 
-export const useScrollProgress = () => {
+type Result = [(el: HTMLElement | SVGElement | null) => void, MotionValue<number>]
+
+/**
+*	Calculates the scroll progress of the ref element as a MotionValue between 0 & 1 (from the el entering the viewport until leaving the viewport.)
+*/
+export const useScrollProgress = (): Result => {
 	const [ref, bounds] = useMeasure()
 	const { scrollY } = useScroll();
 	const value = useTransform(scrollY, [bounds.top - bounds.height, bounds.bottom], [0, 1]);
