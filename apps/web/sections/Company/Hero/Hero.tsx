be9f4b040aaec49040cc7862883098ee85b92941
Luke Smetham <luke@smetham.dev>
2023-06-22 15:21:06 +0200

    Update company hero

--- apps/web/sections/Company/Hero/Hero.tsx
+++ apps/web/sections/Company/Hero/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
 					<TitleBlock 
 						align="center"
 					>
-						<h1>Built by the creators of ML Infrastructure at FAANG.</h1>
+						<h1>Built by the creators of ML Infrastructure at Facebook.</h1>
 						<div className={styles.intro_text}>
 							<p>Fennel is an ex-Facebook/Google team on a mission to enable companies and teams of any size to harness realtime machine learning to build delightful products for their customers.</p>
 						</div>
