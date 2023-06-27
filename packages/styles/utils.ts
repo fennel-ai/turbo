168aca579eb3a22bb019cd20a4a325457f77c926
Luke Smetham <luke@smetham.dev>
2023-06-27 10:42:12 +0200

    util for detecting accessibility of color pairings

--- packages/styles/utils.ts
+++ packages/styles/utils.ts
@@ -30,10 +30,18 @@ export const media = (bp: Breakpoint, type: 'min' | 'max' = 'min', fallback: any
 		return `@media (${type}-width: ${value}rem)`;
 	};
 
+/**
+ * 	Given a color string (hsl, rgb, hex, etc.) and an value between 0 and 1 (0 being fully-opaque and 1 being a solid color)
+ *  sets the alpha channel and returns the modified color string.
+ */
 export const rgba = (color: string, alpha: number) => {
 	if (alpha < 0 || alpha > 1) {
 		throw new Error(`Invalid alpha value ${alpha} provided to rgba()`);
 	}
 
 	return tinycolor(color).setAlpha(alpha).toRgbString();
+}
+
+export const accessibility = (color: string, on_color: string, size: 'small' | 'large' = 'small') => {
+	return tinycolor.isReadable(color, on_color, { level: "AA", size });
 }
