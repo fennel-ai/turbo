8fc67c811dd2cb64f0771508b3b5ee29a7224dbb
Luke Smetham <luke@smetham.dev>
2023-06-28 10:12:57 +0200

    prepare Footer

--- apps/web/components/Footer/Footer.tsx
+++ apps/web/components/Footer/Footer.tsx
@@ -1,20 +1,70 @@
-import clsx from 'clsx';
+import styled from '@emotion/styled';
 import { Container } from 'ui';
 import Logo from 'ui/icons/logo.svg';
-import styles from './Footer.module.scss';
+import { media } from 'styles/utils';
+
+const Root = styled.footer`
+	background-color: ${({ theme }) => theme.surface};
+	border-top: 1px solid ${({ theme }) => theme.border};
+	padding: 2.5rem 0;
+	display: flex;
+	flex-direction: column;
+	gap: 1.5rem;
+	
+	${media('sm')} {
+		padding: 5rem 0;
+	}
+`;
+
+const Wrapper = styled(Container)`
+	display: flex;
+	align-items: center;
+
+	${media('sm', 'max')} {
+		justify-content: center;
+	}
+`;
+
+const Brand = styled.div`
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+	
+	svg {
+		width: 1.5rem;
+		height: 1.5rem;
+	}
+
+	& h3 {
+		margin: 0;
+		font-size: 1rem;
+		line-height: 1.5rem;
+	}
+`;
+
+const Credit = styled(Wrapper)`
+	& p {
+		margin: 0;
+		font-size: 0.875rem;
+		line-height: 1rem;
+		opacity: 64%;
+		color: ${({ theme }) => theme.on_alt};
+		font-variation-settings: "wght" 500;
+	}
+`;
 
 export const Footer = () => {
 	return (
-		<footer className={styles.root}>
-			<Container className={styles.wrapper}>
-				<div className={styles.brand}>
+		<Root>
+			<Wrapper>
+				<Brand>
 					<Logo />
 					<h3>Fennel</h3>
-				</div>
-			</Container>
-			<Container className={clsx(styles.wrapper, styles.credit)}>
+				</Brand>
+			</Wrapper>
+			<Credit>
 				<p>© 2023 Fennel · All Rights Reserved</p>
-			</Container>
-		</footer>
+			</Credit>
+		</Root>
 	);
 };
