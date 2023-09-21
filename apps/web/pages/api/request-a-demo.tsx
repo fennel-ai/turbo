6030272a42bb4feb03ba6beab86b33367f5299ac
Luke Smetham <luke@smetham.dev>
2023-09-21 14:57:35 +0200

    send submissions to hubspot in serverless form handler

--- apps/web/pages/api/request-a-demo.tsx
+++ apps/web/pages/api/request-a-demo.tsx
@@ -1,30 +1,76 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import fetch, { RequestInit } from 'node-fetch';
 import sendMail from '../../emails';
 import RequestADemoInternal from '../../emails/RequestADemoInternal';
 
 export default async function handler(
-	request: NextApiRequest,
-	response: NextApiResponse,
+    request: NextApiRequest,
+    response: NextApiResponse,
 ) {
-	const { body } = request;
+    try {
+        const { body } = request;
 
-	if (!body.name || !body.role || !body.email) {
-		response.status(400).json({
-			body: "Malformed Request Body"
-		});
-	}
+        if (!body.name || !body.role || !body.email) {
+            response.status(400).json({
+                body: "Malformed Request Body"
+            });
+        }
 
-	await sendMail({
-		to: 'hello@fennel.ai',
-		subject: `New Demo Request <${body.email}>`,
-		component: <RequestADemoInternal name={body.name} role={body.role} email={body.email} />
-	});
-	
-	response.status(200).json({
-		body: {
-			"status": "ok"
-		},
-		query: request.query,
-		cookies: request.cookies,
-	});
+        const requestInit: RequestInit = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${process.env.HUBSPOT_API_KEY}`
+            },
+            body: JSON.stringify({
+                fields: [
+                    {
+                        name: 'firstName',
+                        value: body.name
+                    }, 
+                    {
+                        name: 'email',
+                        value: body.email
+                    }, 
+                    // {
+                    //     name: 'company',
+                    //     value: 'Fennel'
+                    // }, 
+                    {
+                        name: 'jobtitle',
+                        value: body.role
+                    }, 
+                    // {
+                    //     name: 'message',
+                    //     value: 'this is an internal test.'
+                    // }
+                ],
+                context: '/request-a-demo'
+            })
+        };
+
+        await fetch(`https://api.hsforms.com/submissions/v3/integration/secure/submit/${process.env.HUBSPORT_PORTAL_ID}/${process.env.HUBSPORT_FORM_ID}`, requestInit);
+
+        await sendMail({
+            to: 'hello@fennel.ai',
+            subject: `New Demo Request <${body.email}>`,
+            component: <RequestADemoInternal name={body.name} role={body.role} email={body.email} />
+        });
+
+        response.status(200).json({
+            body: {
+                "status": "ok"
+            },
+            query: request.query,
+            cookies: request.cookies,
+        });
+    } catch (e) {
+        response.status(500).json({
+            body: {
+                "status": "error"
+            },
+            query: request.query,
+            cookies: request.cookies,
+        });
+    }
 }
