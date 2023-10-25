d9b8cd2fec3a687895577e6b990d0bd4e4ac5a05
Luke Smetham <luke@smetham.dev>
2023-10-25 16:12:29 +0200

    added careers page

---
+++ apps/web/sections/Careers/JobList/JobList.tsx
@@ -0,0 +1,84 @@
+import styled from "@emotion/styled";
+import { Button, Container } from "ui";
+import JobListing from "./JobListing";
+
+const Root = styled.div`
+    position: relative;
+    padding: 3rem 0 5rem 0;
+`;
+
+const Wrapper = styled(Container)`
+	display: grid;
+	grid-template-columns: repeat(12, 1fr);
+    grid-row-gap: 2.5rem;
+`;
+
+const List = styled.div`
+    grid-column: 4 / span 6;
+`;
+
+const ListTitle = styled.div`
+    padding: 0.5rem 1rem;
+    border-bottom: 1px solid ${({ theme }) => theme.border};
+
+    & h4 {
+        color: ${({ theme }) => theme.on_alt};
+        opacity: 0.64;
+        user-select: none;
+        text-transform: uppercase;
+        margin: 0;
+        font-size: 1rem;
+        line-height: 1.5rem;
+    }
+`;
+
+const ApplyCTA = styled.div`
+    grid-column: 4 / span 6;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 2rem;
+    padding: 0 1rem;
+`;
+
+const CTAContent = styled.div`
+    max-width: 24.5rem;
+
+    & h5 {
+        margin: 0;
+        font-size: 1.125rem;
+        line-height: 1.5rem;
+        font-variation-settings: "wght" 600;
+    }
+
+    & p {
+        margin: 0;
+        font-size: 0.875rem;
+        line-height: 1.5rem;
+    }
+`;
+
+const JobList = () => {
+    return <Root>
+        <Wrapper>
+            <List>
+                <ListTitle><h4>Open Roles</h4></ListTitle>
+                <JobListing name="Full-Stack Engineer" location="Remote" modality="Full-Time" href="https://fennel.ai" />
+                <JobListing name="Systems Engineer" location="Bay Area" modality="Full-Time" href="https://fennel.ai" />
+                <JobListing name="Systems Engineer" location="Remote" modality="Full-Time" href="https://fennel.ai" />
+                <JobListing name="Sales Development Representative" location="Remote" modality="Full-Time" href="https://fennel.ai" />
+            </List>
+            <ApplyCTA>
+                <CTAContent>
+                    <h5>
+                        Interested but don't see a role for you?
+                    </h5>
+                    <p>Eos sit qui placeat et id. Velit quis atque non nam. Similique id officia dolor harum vitae rerum.</p>
+                </CTAContent>
+                <Button color="primary" label="hello@fennel.ai" />
+            </ApplyCTA>
+        </Wrapper> 
+    </Root>
+};
+
+export default JobList;
---
+++ apps/web/sections/Careers/JobList/JobListing.tsx
@@ -0,0 +1,176 @@
+import styled from "@emotion/styled";
+import { motion } from 'framer-motion';
+import { useRouter } from "next/router";
+
+import ChevronRightIcon from 'ui/icons/chevron-right.svg';
+import MarkerPinIcon from 'ui/icons/marker-pin.svg';
+
+const Root = styled(motion.div)`
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    user-select: none;
+    cursor: pointer;
+
+    & + & {
+        border-top: 1px solid ${({ theme }) => theme.border};
+    }
+`;
+
+const Meta = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 0;
+
+    & h5 {
+        margin: 0;
+        font-size: 1.125rem;
+        line-height: 1.5rem;
+    }
+
+    & p {
+        margin: 0;
+        font-size: 0.875rem;
+        line-height: 1.5rem;
+    }
+`;
+
+const Details = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+    opacity: 0.5;
+    color: ${({ theme }) => theme.on};
+
+    & svg {
+        width: 0.75rem;
+        height: 0.75rem;
+    }
+
+    & a {
+			position: relative;
+			text-decoration: none;
+			color: ${({ theme }) => theme.on};
+			opacity: 0.8;
+			transition: 160ms opacity ease-out;
+
+			&::after {
+				content: '';
+				position: absolute;
+				bottom: -1px;
+				left: 0;
+				right: 0;
+				height: 1px;
+				background-color: ${({ theme }) => theme.on};
+				opacity: 0.4;
+				transition: 80ms opacity ease-out;
+				z-index: -1;
+			}
+
+			&:hover {
+				opacity: 1;
+
+				&::after {
+					opacity: 0.64;
+				}
+			}
+		}
+`;
+
+const Indicator = styled(motion.div)`
+    display: flex;
+    align-items: center;
+    gap: 0.25rem;
+
+    & span {
+        margin: 0;
+        font-size: 0.875rem;
+        line-height: 1rem;
+        transform: translate3d(0, 8px, 0);
+        opacity: 0;
+    }
+`;
+
+const IconWrapper = styled(motion.div)`
+    width: 1rem;
+    height: 1rem;
+
+    & > svg {
+        width: 1rem;
+        height: 1rem;
+    }
+`;
+
+type JobListingProps = {
+    name: string;
+    href?: string;
+    location: string;
+    modality: string;
+}
+
+const VARIANTS = {
+    apply: {
+        default: {
+            y: 8,
+            opacity: 0,
+        },
+        hovered: {
+            y: 0,
+            opacity: 1,
+            transition: {
+                delay: 0.1,
+                staggerChildren: 0.2,
+            }
+        }
+    },
+    indicator: {
+        span: {
+            default: {
+                y: 8,
+                opacity: 0,
+            },
+            hovered: {
+                y: 0,
+                opacity: 1,
+            }
+        },
+        icon: {
+            default: {
+                opacity: 0.5,
+            },
+            hovered: {
+                opacity: 1,
+            }
+        }
+    }
+}
+
+const JobListing = ({ href, location, modality, name }: JobListingProps) => {
+    const router = useRouter();
+    return (
+        <Root initial="default" whileHover="hovered" onClick={() => href ? router.push(href) : null}>
+            <Meta>
+                <p>{name}</p>
+                <Details>
+                    <MarkerPinIcon />
+                    <p>{location}</p>
+                    <p>·</p>
+                    <p>{modality}</p>
+                    <Details variants={VARIANTS.apply}>
+                        <p>·</p>
+                        <p>Apply at <a href="mailto:hello@fennel.ai" onClick={e => e.stopPropagation()}>hello@fennel.ai</a></p>
+                    </Details>
+                </Details>
+            </Meta>
+            <Indicator>
+                <motion.span variants={VARIANTS.indicator.span}>Learn More</motion.span>
+                <IconWrapper variants={VARIANTS.indicator.icon}>
+                    <ChevronRightIcon />
+                </IconWrapper>
+            </Indicator>
+        </Root>
+    )
+}
+
+export default JobListing;
