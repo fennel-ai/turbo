69723857905e61b5bcd7168dd036bbe97733bec3
Luke Smetham <luke@smetham.dev>
2023-11-06 14:38:04 +0100

    add new button variants

--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -2,16 +2,26 @@ import { MouseEventHandler } from 'react';
 import styled from '@emotion/styled';
 import { stateLayer } from 'styles/utils';
 
+import * as VARIANTS from './variants';
+
 type Props = {
 	ariaLabel?: string;
 	className?: string;
 	color?: 'primary' | 'neutral';
 	onClick?: MouseEventHandler<HTMLButtonElement>;
 	label: string;
+    size?: keyof typeof VARIANTS.SIZE;
+    shape?: keyof typeof VARIANTS.SHAPE;
 	type?: 'button' | 'submit';
+    variant?: keyof typeof VARIANTS.STYLE;
 };
 
-const Root = styled.button<{ color: 'primary' | 'neutral' }>`
+const Root = styled.button<{ 
+    color: 'primary' | 'neutral', 
+    size: keyof typeof VARIANTS.SIZE,
+    shape: keyof typeof VARIANTS.SHAPE,
+    variant: keyof typeof VARIANTS.STYLE 
+}>`
 	border: 0;
 	position: relative;
 	display: flex;
@@ -19,20 +29,20 @@ const Root = styled.button<{ color: 'primary' | 'neutral' }>`
 	align-items: center;
 	justify-content: center;
 	gap: 0.5rem;
-	padding: 0 1rem;
+	padding: 0 0.75rem;
 	font-size: 0.875rem;
-	line-height: 1rem;
-	height: 2.5rem;
+	line-height: 1.5rem;
+    font-variation-settings: "wght" 500;
 	cursor: pointer;
 	user-select: none;
 	overflow: hidden;
 	text-decoration: none;
-	border-radius: 0.5rem;
 	z-index: 0;
-	background-color: ${({ color, theme }) => theme[color].accent};
-	color: ${({ color, theme }) => theme[color].on};
 
-	${stateLayer()};
+    ${props => stateLayer(props.variant === 'ghost' ? 0.04 : 0)};
+    ${(props) => VARIANTS.STYLE[props.variant]}
+    ${(props) => VARIANTS.SHAPE[props.shape]}
+    ${(props) => VARIANTS.SIZE[props.size]};
 `;
 
 export const Button = ({
@@ -41,10 +51,22 @@ export const Button = ({
 	color = 'neutral',
 	label,
 	onClick,
+    size = 'large',
+    shape = 'rounded',
+    variant = 'flat',
 	type = 'button'
 }: Props) => {
 	return (
-		<Root className={className} aria-label={ariaLabel} color={color} onClick={onClick} type={type}>
+        <Root 
+            className={className} 
+            aria-label={ariaLabel} 
+            color={color} 
+            onClick={onClick} 
+            shape={shape} 
+            size={size}
+            type={type}
+            variant={variant} 
+        >
 			{label}
 		</Root>
 	);
