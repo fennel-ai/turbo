a05f90ee1b927af04a63bbecd8a98aa130f2dfad
Luke Smetham <luke@smetham.dev>
2024-02-03 13:06:30 +0100

    New size variants for icon buttons from design system

--- packages/ui/src/IconButton/IconButton.tsx
+++ packages/ui/src/IconButton/IconButton.tsx
@@ -1,51 +1,79 @@
 import { ComponentType, MouseEventHandler } from 'react';
 import styled from '@emotion/styled';
 import { media, stateLayer } from 'styles/utils';
+import { css } from '@emotion/react';
 
-type Props = {
-	ariaLabel?: string;
-	className?: string,
-	icon: ComponentType,
-	onClick?: MouseEventHandler<HTMLButtonElement>,
+export const SIZE = {
+    'small': () => css`
+        width: 1.5rem;
+        height: 1.5rem;
+        border-radius: 50%;
+
+        & svg {
+            width: 1rem;
+            height: 1rem;
+        }
+    `,
+    'default': () => css`
+        width: 2rem;
+        height: 2rem;
+        border-radius: 0.5rem;
+
+        & svg {
+            width: 1.25rem;
+            height: 1.25rem;
+        }
+    `,
+    'large': () => css`
+        width: 2.5rem;
+        height: 2.5rem;
+        border-radius: 0.75rem;
+
+        & svg {
+            width: 1.5rem;
+            height: 1.5rem;
+        }
+    `,
 }
 
-const Root = styled.button`
+const Root = styled.button<{
+    size: keyof typeof SIZE,
+}>`
+    ${(props) => SIZE[props.size]};
 	border: 0;
 	background: none;
 	position: relative;
 	display: flex;
 	align-items: center;
 	justify-content: center;
-	border-radius: 0.5rem;
-	cursor: pointer;
 	color: currentColor; 
 	overflow: hidden;
-	width: 2rem;
-	height: 2rem;
+	cursor: pointer;
 
 	& svg {
+        pointer-events: none;
 		flex-shrink: 0;
-		width: 1.25rem;
-		height: 1.25rem;
+        color: ${({ theme }) => theme.on_alt};
 	}
+
+    &:hover svg {
+        color: ${({ theme }) => theme.on};
+    }
 
 	${stateLayer()};
-
-	${media('md')} {
-		width: 2.5rem;
-		height: 2.5rem;
-		border-radius: 0.75rem
-
-		& svg {
-			width: 1.5rem;
-			height: 1.5rem;
-		}
-	}
 `;
 
-export const IconButton = ({ ariaLabel, className, icon: Icon, onClick }: Props) => {
+type Props = {
+    ariaLabel?: string;
+    className?: string,
+    icon: ComponentType,
+    onClick?: MouseEventHandler<HTMLButtonElement>,
+    size: keyof typeof SIZE,
+}
+
+export const IconButton = ({ ariaLabel, className, icon: Icon, onClick, size = 'default' }: Props) => {
 	return (
-		<Root aria-label={ariaLabel} className={className} onClick={onClick}>
+		<Root aria-label={ariaLabel} className={className} onClick={onClick} size={size}>
 			<Icon />
 		</Root>
 	);
