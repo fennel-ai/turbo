fde203f32f80d93e5060e79d213f7e85f7bffd2f
Luke Smetham <luke@smetham.dev>
2024-02-11 09:52:13 +0100

    Add Sticky to MdxComponents - allows making any arbitrary element stick on the right

--- apps/docs/components/MDXComponents/Sticky.tsx
+++ apps/docs/components/MDXComponents/Sticky.tsx
@@ -3,9 +3,8 @@ import { createPortal } from 'react-dom';
 import { SplitLayoutContext } from 'context/SplitLayoutContext';
 
 
-const Sticky = ({ children }: { children: ReactNode | undefined }) => {
-    const stickyRef = useContext(SplitLayoutContext)
+export const Sticky = ({ children }: { children: ReactNode | undefined }) => {
+    const stickyRef = useContext(SplitLayoutContext);
+    
     return stickyRef ? createPortal(children, stickyRef as HTMLDivElement) : children;
 };
-
-export default Sticky;
--- apps/docs/components/MDXComponents/index.tsx
+++ apps/docs/components/MDXComponents/index.tsx
@@ -4,19 +4,21 @@ import { Image } from './Image';
 import { TitleBlock } from './TitleBlock';
 import { Table } from './Table';
 import { CodeSnippet, type CodeSnippetProps } from './CodeSnippet';
-import Sticky from './Sticky';
+import { Sticky } from './Sticky';
 
 export { Admonition } from './Admonition';
 export { Grid } from './Grid';
 export { PageReference } from './PageReference';
 export { TypesList } from './TypesList';
 export { TypesListRow } from './TypesListRow';
-export {Expandable} from './Expandable';
+export { Expandable } from './Expandable';
+export { Sticky } from './Sticky';
 export { Divider, LeftSection, RightSection } from './Divider';
 
 export const h1 = TitleBlock;
 export const a = Link;
 export const table = Table;
+export const img = Image;
 export const pre = (props: CodeSnippetProps) => {
     return (
         <Sticky>
@@ -34,4 +36,3 @@ export const CodeTabs = (props: { children: ReactNode }) =>  {
         </Sticky>
     );
 }
-export const img = Image;
