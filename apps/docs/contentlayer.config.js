9a60a74d1a6f2b88cc7a89cfcdd3fd933cd336e8
Luke Smetham <luke@smetham.dev>
2024-02-25 17:30:39 +0100

    WIP iterate over head/branch refs to fetch content

--- apps/docs/contentlayer.config.js
+++ apps/docs/contentlayer.config.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import fs from "fs-extra";
 import path from "node:path";
 import { makeSource } from "contentlayer/source-remote-files";
+import { parse as parseYaml } from "yaml";
 
 // Remark/Rehype plugins
 import remarkGfm from "remark-gfm";
@@ -27,23 +28,45 @@ import {
 const CONTENT_DIR = "_content";
 
 const githubSource = async () => {
-  if (process.env.MODE === "EDIT") {
-    console.log(
-      `[Edit Mode]: Content will not be fetched from the content repo`
-    );
     await fs.ensureDir(path.join(process.cwd(), CONTENT_DIR));
-  } else {
-    console.log(`Pulling content from content repo...`);
-    // replace this with octokit:
-    await fetchContent(process.env.GITHUB_TOKEN, CONTENT_DIR);
-  }
 
-  // Move assets to nexts static dir
-  await fs.copy(
-    path.join(process.cwd(), CONTENT_DIR, "assets"),
-    path.join(process.cwd(), "public", "assets"),
-    { overwrite: true }
-  );
+    if (process.env.MODE === "EDIT") {
+        console.log(
+        `[Edit Mode]: Content will not be fetched from the content repo`
+        );
+    } else {
+        await fs.emptyDir(CONTENT_DIR);
+
+        console.log(`Pulling content from content repo...`);
+        await fetchContent(process.env.GITHUB_TOKEN, CONTENT_DIR, [{ name: 'main', head: 'main' }]);
+    }
+
+    // We now have the content in mainDir
+    const versionsManifestStr = await fs.readFile(path.join(CONTENT_DIR, 'main', "versions.yml"), 'utf-8');
+    const versionsManifest = parseYaml(versionsManifestStr);
+
+    const { versions } = versionsManifest;
+
+    // Fetch all other versions listed in the versions manifest
+    await fetchContent(process.env.GITHUB_TOKEN, CONTENT_DIR, versions);
+
+    // Move assets to nexts static dir
+    for await (const version of [{ name: 'main', head: 'main' }, ...versions]) {
+        const assetPath = path.join(
+          process.cwd(),
+          "public",
+          "assets",
+          version.name
+        );
+
+        await fs.ensureDir(assetPath)
+        await fs.copy(
+            path.join(process.cwd(), CONTENT_DIR, version.name, "assets"),
+            assetPath,
+            { overwrite: true }
+        );
+    }
+
   // NOOP We don't need to do anything as we're not subscribing to any data changes right now.
   return () => {};
 };
@@ -53,13 +76,20 @@ export default makeSource({
   contentDirPath: CONTENT_DIR,
   documentTypes: [Page, Config, APIConfig, VersionsManifest],
   contentDirExclude: [
-    ".git",
-    ".gitignore",
-    "docker-compose.yml",
-    "Makefile",
-    "README.md",
-    "algolia.config.json",
-    "deprecated",
+    "main/.git",
+    "main/.gitignore",
+    "main/docker-compose.yml",
+    "main/Makefile",
+    "main/README.md",
+    "main/algolia.config.json",
+    "main/deprecated",
+    "0.20.19/.git",
+    "0.20.19/.gitignore",
+    "0.20.19/docker-compose.yml",
+    "0.20.19/Makefile",
+    "0.20.19/README.md",
+    "0.20.19/algolia.config.json",
+    "0.20.19/deprecated",
   ],
   mdx: {
     remarkPlugins: [
--- apps/docs/contentlayer/fetchContent.js
+++ apps/docs/contentlayer/fetchContent.js
@@ -1,10 +1,11 @@
 import { Octokit } from "octokit";
 import { Readable } from "node:stream";
+import path from 'node:path';
 import fs from "fs-extra";
 import tar from "tar-fs";
 import gunzip from "gunzip-maybe";
 
-const fetchContent = async (token, dir) =>
+const fetchContent = async (token, dir, versions) =>
   new Promise(async (resolve, reject) => {
     if (!token) {
       reject("No github token provided.");
@@ -14,39 +15,42 @@ const fetchContent = async (token, dir) =>
       auth: token,
     });
 
-    // Clear the content dir, this also created the dir if it doesn't exist.
-    await fs.emptyDir(dir);
+    for (const version of versions) {
+        // Fetch the repo from main
+        // (this contains the versions.yml we need to fetch the remaining versions)
+        const outputDir = path.join(dir, version.name);
 
-    // Fetch the repo as a tarball
-    const { data } = await octo.request(
-      "GET /repos/{owner}/{repo}/tarball/{?ref}",
-      {
-        owner: "fennel-ai",
-        repo: "client",
-        ref: "main",
-      }
-    );
+        // Clear the content dir, this also created the dir if it doesn't exist.
+        await fs.emptyDir(outputDir);
 
-    // Create a readable stream from the tar blob
-    const stream = new Readable();
-    stream._read = () => {};
+        const { data } = await octo.request(
+            "GET /repos/{owner}/{repo}/tarball/{?ref}",
+            {
+                owner: "fennel-ai",
+                repo: "client",
+                ref: version.head,
+            }
+        );
 
-    stream.push(Buffer.from(data));
-    stream.push(null);
+        const stream = new Readable();
+        stream._read = () => {};
 
-    // gunzip it and then extract.
-    stream.pipe(gunzip()).pipe(
-      tar.extract('.tmp', {
-        finish: () => {
-			// TODO: THis isn't the most efficient right now, but will need changing either way to support incremental builds
-			// Will circle back when working on support for incremental builds.
-			fs.moveSync('.tmp/docs', dir, { overwrite: true });
-			fs.rmSync('.tmp', { recursive: true });
-			resolve();
-		},
-        strip: 1, // Here we strip the first path segment so that the files are placed directly into the content dir instead of `content_dir/repo_name_with_hash/*`
-      })
-    );
+        stream.push(Buffer.from(data));
+        stream.push(null);
+
+        stream.pipe(gunzip()).pipe(
+            tar.extract(".tmp", {
+            finish: () => {
+                // TODO: THis isn't the most efficient right now, but will need changing either way to support incremental builds
+                // Will circle back when working on support for incremental builds.
+                fs.moveSync(".tmp/docs", outputDir, { overwrite: true });
+                fs.rmSync(".tmp", { recursive: true });
+                resolve();
+            },
+            strip: 1, // Here we strip the first path segment so that the files are placed directly into outputDir instead of `{outputDir}/repo_name_with_hash/*`
+            })
+        );
+    }
   });
 
 export default fetchContent;
