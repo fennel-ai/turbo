2b9d914f0090ea5db73f3c12d813eac78bd72f52
Luke Smetham <luke@smetham.dev>
2024-03-17 13:58:12 +0100

    Fix algolia

--- apps/docs/pages/sitemap.xml.tsx
+++ apps/docs/pages/sitemap.xml.tsx
@@ -1,23 +1,33 @@
 import { allPages } from "contentlayer/generated";
 import { GetServerSideProps } from "next";
 
+/**
+ * After launching the API reference we had an issue where many of the titles were being scraped more than once by Algolia.
+ * This was originally due to having all of the API Reference pages in allPages (this has now changed so that we have allPages and
+ * allAPIPages.) However, because all API Reference pages are the exact same (there is actually only /api-reference/ and everything
+ * else is handled on the client.) this meant that each URL was being indexed by algolia and things were being duplicated.
+ *
+ * e.g. we had `<h1>Client</h1>` appearing for every possible API reference path.
+ * 
+ * Now we can just map over allPages as before, and include /docs/api-reference by hand as the only URL that should be scraped by algolia.
+ */
 function generateSiteMap() {
-	return `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-	<url>
-	<loc>https://fennel.ai/docs</loc>
-	</url>
-	${allPages
-		.filter(({ status }) => status !== 'draft')
-		.map(({ slug }) => {
-			return `
-	<url>
-		<loc>${`https://fennel.ai/docs/${slug}`}</loc>
-	</url>
-	`;
-		})
-		.join('')}
-</urlset>
+	return `
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>https://fennel.ai/docs</loc>
+        </url>
+        ${
+            allPages
+                .filter(({ status }) => status !== 'draft')
+                .map(({ slug }) => {
+                    return `<url><loc>${`https://fennel.ai/docs/${slug}`}</loc></url>`;
+                })
+                .join('')
+        }
+        <url><loc>${`https://fennel.ai/docs/api-reference`}</loc></url>
+    </urlset>
  `;
 }
 
