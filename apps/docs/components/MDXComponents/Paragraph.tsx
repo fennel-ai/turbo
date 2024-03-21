947febba33fb6c4e320208ea09932099ca03c377
Luke Smetham <luke@smetham.dev>
2024-03-21 19:58:13 +0100

    fix fonts

--- apps/docs/components/MDXComponents/Paragraph.tsx
+++ apps/docs/components/MDXComponents/Paragraph.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled';
+import { haskoyVariable } from 'pages/_app';
 
 export const Paragraph = styled.p`
     margin: 0;
     ${({ theme}) => theme.body.default};
+    font-family: ${haskoyVariable.style.fontFamily}, sans-serif;
 `;
