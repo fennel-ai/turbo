d53e4ad021bd81f3f4b7a39da128a164967de996
Luke Smetham <luke@smetham.dev>
2024-06-17 12:30:03 +0200

    endpoint for listing fly apps

---
+++ apps/web/pages/api/playground/apps/index.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { listApps } from "../_utils/fly";
+
+/**
+ * Returns a list of all Fly Apps currently in existence
+ */
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    const { method } = req;
+
+    if (method !== 'GET') {
+        return res.status(404).end();
+    }
+
+    const apps = await listApps();
+
+    res.status(200).json(apps);
+}
