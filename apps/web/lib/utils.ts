a5f8cb0df1b20e2c3ff600ebc6da1843deb9e8ae
Rahul Khanna <r.khanna237@gmail.com>
2024-07-26 02:54:15 +0530

    Fix util import in web app

--- apps/web/lib/utils.ts
+++ apps/web/lib/utils.ts
@@ -1,4 +1,4 @@
-import { allPages, config, Page } from "contentlayer/generated";
+import { Page } from "contentlayer/generated";
 
 export type NavigationPage = {
 	description?: string;
@@ -20,24 +20,3 @@ export type NavigationTree = {
 }
 
 export const shouldPublish = (page: Page): boolean => process.env.NODE_ENV !== 'production' || page.status !== 'draft';
-
-/**
- * Returns the navigation tree from the config.yml file in the content directory.
- */
-export const getNavigation = () => {
-	const { sidebar } = config;
-	return sidebar!.map((section) => ({
-		...section,
-		pages: section.pages.map((slug: string) => {
-			const page = allPages.find((page: Page) => page.slug === slug)!;
-			
-			if (!page) throw new Error(`[config.sidebar]: Could not find page with slug "${slug}" in sidebar config. Make sure that the slug is correct and that the page exists in the "content" directory.`);
-
-			return {
-				title: page.title,
-				slug: page.slug,
-				status: page.status,
-			}
-		}).filter(shouldPublish)
-	}));
-}
